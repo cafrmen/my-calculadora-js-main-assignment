8e21630c4fd425ca58ff42ab43cbb4da7d80831e
cafrmen <cafrmen@gmail.com>
2023-08-09 14:15:42 -0600

    Instrucciones para lograr la asignatura

--- src/app.js
+++ src/app.js
@@ -47,5 +47,22 @@ function operate(button) {
 
 buttons.forEach(button => button.addEventListener('click', () => operate(button)));
 
+/*
 // usar while loop o for loop de dos num y separar numeros y operaciones
-// un numero y una operacion - al oprimir operador asignar variables y hacer una operación y determine una variable
+
+// un numero y una operacion - al oprimir operador asignar variables y hacer
+una operación y determine una variable
+
+// coll cada numero tiene un numero
+simbolo un valor asignado cada simbolo tranformar en la funcion,
+esos símbolos los meto en un grupo en especial para que desaparezca la variable
+esos numeros se vuelven un valor
+loop cada que oprima una operación, se vuelve var 1
+se asignan variables 1+1
+cuando se oprima igual ya se evalua, que aparezca el resultado de toda la cadena
+primera + cero o * 1
+
+// evaluar directamente, es decir cada que se oprime un boton va a realizar una operación,
+solo toma un numero y una operacion
+solo en igual debe haber dos numeros sino, deshabilitar
+*/
