b18e3b52cd5e05f36a55ad7521c21ad0e20cd265
cafrmen <cafrmen@gmail.com>
2023-08-11 19:03:06 -0600

    A grammar issue

--- src/app.js
+++ src/app.js
@@ -176,5 +176,5 @@ document.addEventListener('keyup', (event) => {
     var name = event.key;
     var code = event.code;
     // Alert the key name and key code on keydown
-    alert(`Key pressed ${name} \r\n Key code value: ${code}`);
+    alert(`Key pressed: ${name} \r\n Key code value: ${code}`);
   }, false);
